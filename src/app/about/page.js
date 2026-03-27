1f324680eb489ce100b8f1a123a6378c6eed1ad7
Zeane Denel <zeane.gon@gmail.com>
2026-03-27 23:18:59 +0800

    back button for About Page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function AboutPage() {
+  const router = useRouter();
+
   return (
     <div>
       <h1>About Us</h1>
       <h1>Learn More</h1>
+
+      <button onClick={() => router.back()}>
+        Go Back
+      </button>
     </div>
   );
 }
