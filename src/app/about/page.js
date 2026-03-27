6dde099986d1f560927322dfc9f2ee7d29b9f91a
Zeane Denel <zeane.gon@gmail.com>
2026-03-27 20:52:26 +0800

    about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,1 +1,13 @@
 //Zeane Capuy
+
+"use server";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function Logout() {
+  const cookieData = await cookies();
+  cookieData.delete("login");
+
+  redirect("/");
+}
